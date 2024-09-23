fa66e4d6721f1417446e8fa0cfb4dadebf4cb08a
CJM <130544779+mianmian11111@users.noreply.github.com>
2024-09-23 09:42:25 +0800

    Update attacked_csv_to_txt.py

--- gradmask/attacked_csv_to_txt.py
+++ gradmask/attacked_csv_to_txt.py
@@ -1,20 +1,24 @@
-import pandas as pd
+import csv
 
-# 读取 CSV 文件
-csv_file = 'result_log/sst2_bert/attack-len128-epo10-batch32/attack_results.csv'  # 替换为你的 CSV 文件路径
-df = pd.read_csv(csv_file)
+# CSV 文件路径
+csv_file_path = '/share/home/u2315363122/MI4D/mi4d-j/mi4d-j/gradmask/result_log/sst2_bert/attack-len128-epo10-batch32/deepwordbug/test/attack_results.csv'
+# TXT 文件路径
+txt_file_path = '/share/home/u2315363122/MI4D/mi4d-j/mi4d-j/gradmask/dataset/sst2/att_test.txt'
 
-# 筛选出 result_type 为 "Successful" 的行
-successful_attacks = df[df['result_type'] == 'Successful']
-
-# 提取 perturbed_text 和 ground_truth_output 列
-perturbed_texts = successful_attacks['perturbed_text']
-ground_truth_outputs = successful_attacks['ground_truth_output'].astype(int)
-
-# 将结果保存到 TXT 文件中
-txt_file = 'dataset/sst2/attacked_succ.txt'  # 输出的 TXT 文件路径
-with open(txt_file, 'w') as f:
-    for text, output in zip(perturbed_texts, ground_truth_outputs):
-        f.write(f"{text}\t{output}\n")
-
-print(f"Successfully saved perturbed_text and ground_truth_output to {txt_file}")
+# 打开 CSV 文件并读取数据
+with open(csv_file_path, mode='r', encoding='utf-8') as csv_file:
+    # 创建 CSV 读取器
+    csv_reader = csv.DictReader(csv_file)
+    
+    # 打开 TXT 文件准备写入
+    with open(txt_file_path, mode='w', encoding='utf-8') as txt_file:
+        # 遍历 CSV 文件中的每一行
+        for row in csv_reader:
+            # 提取 'perturbed_text' 和 'original_output' 列
+            perturbed_text = row['perturbed_text']
+            original_output = row['original_output']
+            # 删除 perturbed_text 中的 '[[', ']]'
+            perturbed_text = perturbed_text.replace('[[', '').replace(']]', '')
+            
+            # 将它们用制表符分隔后写入 TXT 文件，每个样本一行
+            txt_file.write(f"{perturbed_text}\t{original_output}\n")
